077897ece7050d50fdf85e3c3ef4ac204d45ea24
まぐにあ <magnia@mail.mag-neko.net>
2024-06-23 09:58:07 +0900

    パスを通した

--- src/flutter-sdk-android/install.sh
+++ src/flutter-sdk-android/install.sh
@@ -10,10 +10,12 @@ apt install -y \
     android-sdk \
     openjdk-17-jdk
 
-curl "${ANDROID_SDK_CMDLINE_TOOLS_URL}"
+curl -o "${ANDROID_SDK_CMDLINE_TOOLS_FILENAME}" "${ANDROID_SDK_CMDLINE_TOOLS_URL}"
 unzip "${ANDROID_SDK_CMDLINE_TOOLS_FILENAME}"
 mkdir -p /usr/lib/android-sdk/cmdline-tools/latest
 mv cmdline-tools/* /usr/lib/android-sdk/cmdline-tools/latest
 rm "${ANDROID_SDK_CMDLINE_TOOLS_FILENAME}"
 rm -r cmdline-tools
+export ANDROID_HOME="/usr/lib/android-sdk"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools/bin"
 yes y | sdkmanager "platform-tools" "platforms;android-33" "build-tools;33.0.2"
